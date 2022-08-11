695c8284c0bcb93d78fe7efe42101175989e7925
thewhobox <45664417+thewhobox@users.noreply.github.com>
2022-08-11 17:07:55 +0200

    Added pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,116 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include "pins.h"
 
 const byte addr = 0x37;
 
 int timedelay = 100;
-const int led = 3;
-const int w11 = 7;
-const int w12 = 8;
-const int w21 = 14;
-const int w22 = 15;
 
-void DataReceive(int numBytes) { }
-void DataRequest() { }
-
-void setup() {
-  Wire.begin(addr);
-  Wire.onReceive(DataReceive);
-  Wire.onRequest(DataRequest);
-  
-  pinMode(led, OUTPUT);
-  pinMode(w11, OUTPUT);
-  pinMode(w12, OUTPUT);
-  pinMode(w21, OUTPUT);
-  pinMode(w22, OUTPUT);
-
-
-  for(int i = 0; i < 5; i++) {
-    digitalWrite(led, HIGH);
-    delay(1000);
-    digitalWrite(led, LOW);
-    delay(500);
-  }
-
+enum ElementType
+{
+    Weiche,
+    Signal,
+    Block
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if(Wire.available()) {
-    digitalWrite(led, HIGH);
-    int data = Wire.read();
-    int pin;
+struct Element
+{
+    const int pin1,
+    const int pin2,
+    const ElementType type
+};
+
+const Element Elements[] = {
+    {
+        .pin1 = PIN_D15,
+        .pin2 = PIN_D16,
+        .type = ElementType.Weiche
+    },
+    {
+        .pin1 = PIN_D5,
+        .pin2 = PIN_D6,
+        .type = ElementType.Weiche
+    },
+    {
+        .pin1 = PIN_D7,
+        .pin2 = PIN_D8,
+        .type = ElementType.Signal
+    },
+    {
+        .pin1 = PIN_D3,
+        .pin2 = PIN_D4,
+        .type = ElementType.Signal
+    },
+};
+
+const int led1 = PIN_D14;
+const int led2 = PIN_D13;
+
+void DataReceive(int numBytes) {}
+void DataRequest() {}
+
+void setup()
+{
+    Wire.begin(addr);
+    Wire.onReceive(DataReceive);
+    Wire.onRequest(DataRequest);
+
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
     
-    switch(data) {
-      case 0:
-        pin = w11;
-        break;
-      case 1:
-        pin = w12;
-        break;
-      case 2:
-        pin = w21;
-        break;
-      case 3:
-        pin = w22;
-        break;
+    int counter = size(Elements) / size(Elements[0]);
+    for(int i = 0; i < counter; i++)
+    {
+        pinMode(Elements[i].pin1, OUTPUT);
+        pinMode(Elements[i].pin2, OUTPUT);
     }
 
-    digitalWrite(pin, HIGH);
-    delay(timedelay);
-    digitalWrite(pin, LOW);
-    digitalWrite(led, LOW);
-  }
+
+    for (int i = 0; i < 4; i++)
+    {
+        digitalWrite(led1, HIGH);
+        delay(1000);
+        digitalWrite(led1, LOW);
+        delay(500);
+    }
+
+    digitalWrite(led2, HIGH);
+    delay(1000);
+    digitalWrite(led2, LOW);
+    delay(500);
+    
+}
+
+void loop()
+{
+    if (Wire.available())
+    {
+        digitalWrite(led1, HIGH);
+        int data = Wire.read();
+        int index = data >> 1;
+        int state = data & 0x01;
+
+        Element ele = Elements[index];
+
+        switch(ele.type)
+        {
+            case ElementType.Weiche:
+            {
+                int pin = state ? ele.pin2 : ele.pin1;
+                digitalWrite(pin, HIGH);
+                delay(timedelay);
+                digitalWrite(pin, LOW);
+                break;
+            }
+
+            case ElementType.Signal:
+            {
+                digitalWrite(state ? ele.pin1 : ele.pin2, LOW);
+                digitalWrite(state ? ele.pin2 : ele.pin1, HIGH);
+                break;
+            }
+        }
+
+        digitalWrite(led1, LOW);
+    }
 }
---
+++ src/pins.h
@@ -0,0 +1,51 @@
+//https://github.com/MCUdude/MiniCore/blob/master/avr/variants/standard/pins_arduino.h#L154
+/*
+#define PIN_PD0 0
+#define PIN_PD1 1
+#define PIN_PD2 2
+#define PIN_PD3 3
+#define PIN_PD4 4
+#define PIN_PD5 5
+#define PIN_PD6 6
+#define PIN_PD7 7
+#define PIN_PB0 8
+#define PIN_PB1 9
+#define PIN_PB2 10
+#define PIN_PB3 11
+#define PIN_PB4 12
+#define PIN_PB5 13
+#define PIN_PC0 14 // A0
+#define PIN_PC1 15 // A1
+#define PIN_PC2 16 // A2
+#define PIN_PC3 17 // A3
+#define PIN_PC4 18 // A4
+#define PIN_PC5 19 // A5
+#define PIN_PB6 20 // XTAL1
+#define PIN_PB7 21 // XTAL2
+#define PIN_PC6 22 // RESET
+#define PIN_A6 (6)
+#define PIN_A7 (7)
+*/
+
+#define PIN_D1 PIN_PC1
+#define PIN_D2 PIN_PC0
+#define PIN_D3 PIN_PB1
+#define PIN_D4 PIN_PD2
+#define PIN_D5 PIN_PB5
+#define PIN_D6 PIN_PD6
+#define PIN_D7 PIN_PD7
+#define PIN_D8 PIN_PB0
+#define PIN_D9 PIN_PC3
+#define PIN_D10 PIN_PC2
+#define PIN_D11 PIN_PD1
+#define PIN_D12 PIN_PD0
+#define PIN_D13 PIN_PD4
+#define PIN_D14 PIN_PD3
+#define PIN_D15 PIN_PB6
+#define PIN_D16 PIN_PB7
+#define PIN_D17 PIN_A6
+#define PIN_D18 PIN_PB2
+#define PIN_D19 PIN_A7
+
+#define PIN_SDA PIN_PC4
+#define PIN_SCL PIN_PC5
